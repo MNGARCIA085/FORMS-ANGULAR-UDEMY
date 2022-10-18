3f2a186c8c906b93b791e0f5d5f341434070e7c5
MNGARCIA085 <mngarcia085@gmail.com>
2022-10-18 20:16:15 +0200

    corregir bug

--- formulariosApp/src/app/app.module.ts
+++ formulariosApp/src/app/app.module.ts
@@ -4,16 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { BasicosComponent } from './templates/basicos/basicos.component';
-import { DinamicosComponent } from './templates/dinamicos/dinamicos.component';
-import { SwitchesComponent } from './templates/switches/switches.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicosComponent,
-    DinamicosComponent,
-    SwitchesComponent
   ],
   imports: [
     BrowserModule,
