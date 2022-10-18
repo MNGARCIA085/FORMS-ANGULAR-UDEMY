045c099a5860395781a09aba56f8761b6b943696
MNGARCIA085 <mngarcia085@gmail.com>
2022-10-18 02:24:46 +0200

    inicio

---
+++ formulariosApp/src/app/reactive-routing/reactive-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveRoutingRoutingModule } from './reactive-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveRoutingRoutingModule
+  ]
+})
+export class ReactiveRoutingModule { }
