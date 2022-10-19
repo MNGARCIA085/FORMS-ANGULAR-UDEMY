93eea013ef43b49371652c2c28e19ea76c26e4a5
MNGARCIA085 <mngarcia085@gmail.com>
2022-10-19 23:28:39 +0200

    fin forms reactivos

--- formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required,Validators.minLength(3)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]]
+  })
+
+  constructor(private fb:FormBuilder) { }
+
 
   ngOnInit(): void {
+    // valores iniciales del form; los que yo quiera
+    this.miFormulario.reset({
+      nombre:'algo',
+      precio:12
+    })
   }
+
+
+  campoEsValido(campo:string){
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+
+  guardar(){
+
+    console.log('valores',this.miFormulario.value);
+
+    if (this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched(); // para que se muestren los errores
+      return
+    }
+
+    // aquí teóricamente debería guardar los datos ...
+
+    // reset
+    this.miFormulario.reset();
+
+  }
+
+  
+
+
 
 }
--- formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required,Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['death note',Validators.required],
+      ['spx x family',Validators.required]
+    ], Validators.required)
+  })
+
+
+  nuevoFavorito: FormControl = this.fb.control('',Validators.required);
+
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+
+  campoEsValido(campo:string){
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched
+  }
+
+
+  guardar(){
+    if (this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched(); // para que se muestren los errores
+      return
+    }
+    // aquí teóricamente debería guardar los datos ...
+    // reset
+    this.miFormulario.reset();
+  }
+
+
+  agregarFavorito(){
+    if (this.nuevoFavorito.invalid){
+      return;
+    }
+
+    // inserto
+    this.favoritosArr.push(this.fb.control(this.nuevoFavorito.value,Validators.required));
+
+    // reseteo
+    this.nuevoFavorito.reset();
+
+  }
+
+
+  eliminar(i: number){
+    this.favoritosArr.removeAt(i);
+  }
+
 
 }
