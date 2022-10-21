48281e565dd9211d76062f792e78c53a7f48104d
MNGARCIA085 <mngarcia085@gmail.com>
2022-10-21 22:09:22 +0200

    vals. asíncronas

--- formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['',[Validators.required, Validators.pattern(this.validationService.nombreApellidoPattern)]],
-    email: ['',[Validators.required, Validators.pattern(this.validationService.emailPattern)]],
+    email: ['',[Validators.required, Validators.pattern(this.validationService.emailPattern)],[ this.emailValidator ] ],
     username: ['',[Validators.required, this.validationService.noPuedeSerStrider]],
     password: ['', [Validators.required, Validators.minLength(4)]],
     password2: ['', [Validators.required, Validators.minLength(4)]],
@@ -23,17 +24,31 @@ export class RegistroComponent implements OnInit {
   )
 
   constructor(private fb: FormBuilder,
-              private validationService: ValidatorService) { }
+              private validationService: ValidatorService,
+              private emailValidator: EmailValidatorService) { }
 
   campoNoValido(campo: string){
     return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
   }
 
+
+  get emailErrorMsg(): string {
+    
+    const errors = this.miFormulario.get('email')?.errors;
+    if ( errors?.['required'] ) {
+      return 'Email es obligatorio';
+    } else if ( errors?.['pattern'] ) {
+      return 'El valor ingresado no tiene formato de correo';
+    } else if ( errors?.['emailTomado'] ) {
+      return 'El email ya fue tomado';
+    }
+
+    return '';
+  }
+
+
+
   ngOnInit(): void {
-    this.miFormulario.reset({
-      nombre:'nico',
-      email:'algo@hotmail.com'
-    })
   }
 
 
